abf32abbf4b2f3e0f526b41993e18e85cca28f9b
jamietech <jamie@jamietech.co.cc>
2012-04-12 16:05:29 +1000

    Update, update, update!

--- src/tk/nekotech/hardcore/Hardcore.java
+++ src/tk/nekotech/hardcore/Hardcore.java
@@ -1,36 +1,27 @@
 package tk.nekotech.hardcore;
 
-import java.util.logging.Logger;
-
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.*;
 
 public class Hardcore extends JavaPlugin {
-
-	public Logger log = Logger.getLogger("Minecraft");
-	PluginDescriptionFile pdfFile = this.getDescription();
+	Listen listen = new Listen(this);
 	
 	@Override
 	public void onDisable() {
-		log.info("[Hardcore] Disabled!");
+		
 	}
 
 	@Override
 	public void onEnable() {
-		log.info("[Hardcore] Enabled!");
-		
-		final PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvents(new Listen(this), this);
+		getServer().getPluginManager().registerEvents(listen, this);
 	}
 		
 	public void announceDeath(String dead, String message) {
 		   for (final Player player : this.getServer().getOnlinePlayers()) {
 			   player.sendMessage(ChatColor.YELLOW + "RIP, " + dead + "!");
 		   }
-		   log.info("[Hardcore] " + dead + " died a horrible death (" + message + ") and was banned!");
+		   getLogger().info("[jtHardcore] " + dead + " died a horrible death (" + message + ") and was banned!");
 	   }
 
 }
--- src/tk/nekotech/hardcore/Listen.java
+++ src/tk/nekotech/hardcore/Listen.java
@@ -1,10 +1,8 @@
 package tk.nekotech.hardcore;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class Listen implements Listener {
@@ -14,19 +12,14 @@ public class Listen implements Listener {
 		this.hardcore = hardcore;
 	}
 	
-
-	@EventHandler (priority = EventPriority.HIGH)
-	public void deathListener(EntityDeathEvent event) {
-		if (event instanceof PlayerDeathEvent) {
-			PlayerDeathEvent playerEvent = (PlayerDeathEvent)event;
-			final Player player = (Player)playerEvent.getEntity();
-			if (!player.hasPermission("jthardcore.protect")) {
-				player.getWorld().strikeLightningEffect(player.getLocation());
-				player.getWorld().strikeLightningEffect(player.getLocation());
-				hardcore.announceDeath(player.getName().toString(), playerEvent.getDeathMessage().toString());
-				player.kickPlayer("Banned: You died on hardcore mode.");
-				player.setBanned(true);
-			}
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		if (!event.getEntity().hasPermission("jthardcore.protect")) {
+				event.getEntity().getWorld().strikeLightningEffect(event.getEntity().getLocation());
+				event.getEntity().getWorld().strikeLightningEffect(event.getEntity().getLocation());
+				hardcore.announceDeath(event.getEntity().getName().toString(), event.getDeathMessage().toString());
+				event.getEntity().kickPlayer("Banned: You died on hardcore mode.");
+				event.getEntity().setBanned(true);
 		}
 	}
 
